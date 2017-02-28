33f98360c5b6268e01eea33c8bd293baca0e4a45
Mathieu Comandon <strycore@gmail.com>
2017-02-28 14:17:59 -0800

    Display warning for ChoiceFields with too many choices

--- shuup/admin/forms/_base.py
+++ shuup/admin/forms/_base.py
@@ -5,12 +5,16 @@
 #
 # This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
+import logging
 from django.forms import ModelChoiceField
 from filer.fields.file import AdminFileWidget
 
 from shuup.admin.forms.widgets import FileDnDUploaderWidget
 from shuup.admin.utils.permissions import user_has_permission
 from shuup.utils.multilanguage_model_form import MultiLanguageModelForm
+
+LOGGER = logging.getLogger(__name__)
+MAX_RECOMMENDED_CHOICES = 100
 
 
 class ShuupAdminForm(MultiLanguageModelForm):
@@ -30,6 +34,11 @@ class ShuupAdminForm(MultiLanguageModelForm):
                 user = getattr(self.request, "user", None)
                 if not user:
                     return
+
+                choices_count = actual_field.queryset.count()
+                if choices_count > MAX_RECOMMENDED_CHOICES:
+                    LOGGER.warning("Field %s in %s has %d choices, this might slow down the view",
+                                   actual_field.label, self.__class__.__name__, choices_count)
                 actual_field.widget.choices = [
                     (item.pk, item) for item in actual_field.queryset
                     if user_has_permission("view", self.request.user, item)
